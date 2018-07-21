1dbbff478620942349332569ae9fb002d6789a43
Domino <zhikhanastasya@gmail.com>
2018-07-21 15:32:34 +0300

    Add file

---
+++ Task1.Logic/TimeOutArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1.Logic
+{
+    public class TimeOutArgs : EventArgs
+    {
+        public TimeOutArgs(int hours, int minutes, int seconds)
+        {
+            if (hours < 0)
+            {
+                throw new ArgumentException($"The parametr {nameof(hours)} can't consist negative value!");
+            }
+
+            if (minutes < 0)
+            {
+                throw new ArgumentException($"The parametr {nameof(minutes)} can't consist negative value!");
+            }
+
+            if (seconds < 0)
+            {
+                throw new ArgumentException($"The parametr {nameof(seconds)} can't consist negative value!");
+            }
+
+            Message = $"Timer is on {nameof(hours)} : {nameof(minutes)} : {nameof(seconds)}.";
+        }
+
+        public string Message { get; set; }
+    }
+}
