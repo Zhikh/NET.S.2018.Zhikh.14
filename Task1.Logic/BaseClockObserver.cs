cc6e265b06ee753923180d63bfed25bfb6be10d7
Domino <zhikhanastasya@gmail.com>
2018-07-21 16:16:53 +0300

    Add file

---
+++ Task1.Logic/BaseClockObserver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task1.Logic
+{
+    public abstract class BaseClockObserver
+    {
+        /// <summary>
+        /// Add new listener
+        /// </summary>
+        /// <param name="clock"> Object for listening </param>
+        public void Register(Clock clock)
+        {
+            if (clock == null)
+            {
+                throw new ArgumentNullException($"Paramentr {nameof(clock)} can't be null!");
+            }
+
+            clock.TimeOutChange += TimeOutChanged;
+        }
+
+        /// <summary>
+        /// Delete listener
+        /// </summary>
+        /// <param name="clock"> Object for listening </param>
+        public void UnRegister(Clock clock)
+        {
+            if (clock == null)
+            {
+                throw new ArgumentNullException($"Paramentr {nameof(clock)} can't be null!");
+            }
+
+            clock.TimeOutChange -= TimeOutChanged;
+        }
+
+        /// <summary>
+        /// Execute when clock time out event is happened
+        /// </summary>
+        /// <param name="sender"> Object who created event </param>
+        /// <param name="e"> Event inforfation </param>
+        protected abstract void TimeOutChanged(object sender, TimeOutArgs e);
+    }
+}
---
+++ Task1.UI.ConsoleApp/FakeObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Task1.Logic;
+
+namespace Task1.UI.ConsoleApp
+{
+    public class FakeObserver : BaseClockObserver
+    {
+        protected sealed override void TimeOutChanged(object sender, TimeOutArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
