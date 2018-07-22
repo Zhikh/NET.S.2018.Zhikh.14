6d9a6b725120f3af9fc2ec92a421d44cd9a19ac2
Domino <zhikhanastasya@gmail.com>
2018-07-22 15:39:06 +0300

    Add files

---
+++ Task4/Task4.Logic/FibonacciGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task4.Logic
+{
+    public static class FibonacciGenerator
+    {
+        private static readonly double _sqrtFromFive;
+
+        static FibonacciGenerator()
+        {
+            _sqrtFromFive = Math.Sqrt(5);
+        }
+
+        /// <summary>
+        /// Find Fibonacci's sequence from start index to index = count - 1
+        /// </summary>
+        /// <param name="startIndex"> Degree of Binet value </param>
+        /// <param name="count"> Length of sequence</param>
+        /// <returns> Sequence </returns>
+        /// <exception cref="ArgumentException"> When count is less or is zero </exception>
+        public static int[] Generate(int startIndex, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentException($"The parameter {nameof(count)} can't be negative!");
+            }
+
+            if (count == 0)
+            {
+                throw new ArgumentException($"The parameter {nameof(count)} can't be zero!");
+            }
+
+            int[] array = new int[count];
+            double firstExpression = (1 + _sqrtFromFive) / 2;
+            double secondExpression = (1 - _sqrtFromFive) / 2;
+
+            int sign;
+            int index;
+            for (int i = 0; i < count; i++)
+            {
+                sign = 1;
+                index = startIndex;
+
+                if (startIndex < 0)
+                {
+                    index *= -1;
+                    if (index % 2 == 0)
+                    {
+                        sign = -1;
+                    }
+                }
+
+                array[i] = sign * (int)((Math.Pow(firstExpression, index) - Math.Pow(secondExpression, index)) / _sqrtFromFive);
+                startIndex++;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Task4/Task4.Tests/FibonacciGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Task4.Logic;
+
+namespace Task4.Tests
+{
+    [TestFixture]
+    public class FibonacciGeneratorTests
+    {
+        [TestCase(2, 0)]
+        [TestCase(3, -1)]
+        public void Generate_UncorrectData_ArgumentException(int startIndex, int count)
+            => Assert.Catch<ArgumentException>(() => FibonacciGenerator.Generate(startIndex, count));
+
+        [TestCase(0, 1, ExpectedResult = new int[] { 0 })]
+        [TestCase(-1, 1, ExpectedResult = new int[] { 1 })]
+        [TestCase(-2, 1, ExpectedResult = new int[] { -1 })]
+        [TestCase(0, 5, ExpectedResult = new int[] { 0, 1, 1, 2, 3 })]
+        [TestCase(0, 5, ExpectedResult = new int[] { 0, 1, 1, 2, 3 })]
+        [TestCase(-3, 8, ExpectedResult = new int[] { 2, -1, 1, 0, 1, 1, 2, 3 })]
+        [TestCase(-10, 6, ExpectedResult = new int[] { -55, 34, -21, 13, -8, 5 })]
+        public int[] Generate_CorrectData_CorrectResult(int startIndex, int count) 
+            => FibonacciGenerator.Generate(startIndex, count);
+    }
+}
