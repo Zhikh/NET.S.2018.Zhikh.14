ab32650942a5f3ee74c3805204d619d4b9757948
Domino <zhikhanastasya@gmail.com>
2018-07-22 14:36:20 +0300

    Refactor (Task2.Logic)

--- Task2/Task2.Logic/BinarySearcher.cs
+++ Task2/Task2.Logic/BinarySearcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Task2.Logic
 {
@@ -11,30 +12,55 @@ namespace Task2.Logic
         /// <param name="array"> Unsorted/sorted array (it's not matter) </param>
         /// <param name="value"> Value for searching </param>
         /// <returns> Index of value in array </returns>
+        /// <exception cref="ArgumentNullException"> When array or comparer are null </exception>
         public int SearchByBinary(T[] array, T value, IComparer<T> comparer)
         {
+            if (array == null)
+            {
+                throw new ArgumentNullException($"The {nameof(array)} parameter can't be null!");
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException($"The {nameof(comparer)} parameter can't be null!");
+            }
+
             int left = 0;
             int right = array.Length;
             int middle;
 
-            while (true)
+            while (left < right)
             {
                 middle = left + (right - left) / 2;
 
-                if (comparer.Compare(array[middle], value) == 0)
+                if (comparer.Compare(array[left], value) == 0)
                 {
-                    return middle;
+                    return left;
                 }
 
-                if (comparer.Compare(array[middle], value) > 0)
+                if (comparer.Compare(array[middle], value) == 0)
                 {
-                    right = middle;
+                    if (middle == left + 1)
+                    {
+                        return middle;
+                    }
+
+                    right = middle + 1;
                 }
                 else
                 {
-                    left = middle + 1;
+                    if (comparer.Compare(array[middle], value) > 0)
+                    {
+                        right = middle;
+                    }
+                    else
+                    {
+                        left = middle + 1;
+                    }
                 }
             }
+
+            return -1;
         }
     }
 }
