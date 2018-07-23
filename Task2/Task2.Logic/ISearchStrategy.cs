17a014139fb351bd697d45b4ddddc383f7063d6c
Domino <zhikhanastasya@gmail.com>
2018-07-23 17:53:23 +0300

    Delete files

--- Task2/Task2.Logic/ISearchStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Task2.Logic
-{
-    public interface ISearchStrategy<T>
-    {
-        int SearchByBinary(T[] array, T value, IComparer<T> comparer);
-    }
-}
